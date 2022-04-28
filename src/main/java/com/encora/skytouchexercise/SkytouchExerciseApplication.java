d1e09f022ecb737b1445d81063476376e650a16a
moninam <moninambri@gmail.com>
2022-04-28 18:11:06 -0500

    Project Base

---
+++ src/main/java/com/encora/skytouchexercise/SkytouchExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.encora.skytouchexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkytouchExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkytouchExerciseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/encora/skytouchexercise/SkytouchExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.encora.skytouchexercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkytouchExerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
