1eed16f98e22c7b7cda219be40e17c5d9308a1f8
moninam <moninambri@gmail.com>
2022-04-28 19:36:06 -0500

    Project Base Microservice App

---
+++ microservice/src/main/java/com/encora/sktmicroservice/SktMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.encora.sktmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SktMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SktMicroserviceApplication.class, args);
+	}
+
+}
